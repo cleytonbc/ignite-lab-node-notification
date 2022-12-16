e593ae956589ba7f747eb81dd14975c2a0aa42a9
Cleyton B. C <cleytonbc@hotmail.com>
2022-12-16 20:30:06 -0300

    incluido os providers no http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,20 @@ import { DatabaseModule } from '../database/database.module';
 
 import { SendNotification } from '../../application/use-cases/send-notification';
 import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { ReadNotification } from '@application/use-cases/read.notification';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    UnreadNotification,
+    ReadNotification,
+    GetRecipientNotifications,
+  ],
 })
 export class HttpModule {}
