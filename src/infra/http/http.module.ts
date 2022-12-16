1efc0f65b4551f9f29e7f4886d4e1a19ff10e06c
Cleyton B. C <cleytonbc@hotmail.com>
2022-12-16 20:31:28 -0300

    incluido os providers no http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -7,6 +7,7 @@ import { CancelNotification } from '@application/use-cases/cancel-notification';
 import { UnreadNotification } from '@application/use-cases/unread-notification';
 import { ReadNotification } from '@application/use-cases/read.notification';
 import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notification';
 
 @Module({
   imports: [DatabaseModule],
@@ -17,6 +18,7 @@ import { GetRecipientNotifications } from '@application/use-cases/get-recipient-
     UnreadNotification,
     ReadNotification,
     GetRecipientNotifications,
+    CountRecipientNotifications,
   ],
 })
 export class HttpModule {}
